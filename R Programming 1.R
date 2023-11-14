c509de59d0c1c40dfc799a404066ebad63e793d6
rudrastogi <128589996+rudrastogi@users.noreply.github.com>
2023-11-14 23:14:54 +0530

    Add files via upload

---
+++ R Programming 1.R
@@ -0,0 +1,26 @@
+makeCacheMatrix <- function(x = matrix()) {
+  matrizinversa <- NULL
+  set <- function( y ){
+    x <<- y
+    matrizinversa <<- NULL
+  }
+  get <- function() ( x )
+  setInversa <- function( calculoinversa ) ( matrizinversa <<- calculoinversa )
+  getInversa <- function() ( matrizinversa )
+  list( set = set, get = get, setInversa = setInversa, getInversa = getInversa )
+  
+}
+
+
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  solinversa <- x$getInversa()
+  if (!is.null(solinversa)) {
+    message( "getting cached data" )
+    return( solinversa )
+  }
+  data <- x$get()
+  solinversa <- solve(data, ...)
+  x$setInversa(solinversa)
+  solinversa
+}
